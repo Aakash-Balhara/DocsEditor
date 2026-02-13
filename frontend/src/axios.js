55d187dff984258d04cd336d563735ce3c0921cc
Aakash-Balhara <aakashbalhara005@gmail.com>
2026-02-13 16:02:13 +0530

    axios change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3300', // Replace with your backend URL
+  baseURL: 'https://docseditor-cdrg.onrender.com', // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
